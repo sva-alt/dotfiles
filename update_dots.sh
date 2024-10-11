b8a7b1cfee4d5d00415086ef017b8949bbdbe775
sva-alt <175239487+sva-alt@users.noreply.github.com>
2024-10-10 21:23:16 -0500

    delete ruler in vim conf and alter update script

--- update_dots.sh
+++ update_dots.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 
-#This is the folder where I have stored my dotfiles repo, you can change this variable to the absolute path of your dotfiles repo.
-dotfiles_folder=~/Documents/notes/1_proyects/dotfiles
+# cd into path were script is executed
+if [[ $BASH_SOURCE = */* ]]; then
+    cd -- "${BASH_SOURCE%/*}/" || exit
+fi 
 
 #update .bashrc
-cp ${dotfiles_folder}/bash/.bashrc ~
+cp bash/.bashrc ~
 
 #update .vimrc
-cp ${dotfiles_folder}/vim/.vimrc ~
+cp vim/.vimrc ~
 
 #update .tmux.conf
-cp ${dotfiles_folder}/tmux/.tmux.conf ~
+cp tmux/.tmux.conf ~
 
 #update nvim files
-cp -r ${dotfiles_folder}/nvim/ ~/.config/ 
+cp -r nvim/ ~/.config/ 
 
 echo done
 
