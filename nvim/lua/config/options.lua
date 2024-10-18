54b2ecabc531c4825b4e14a7d488d40077fd84cf
sva-alt <175239487+sva-alt@users.noreply.github.com>
2024-10-17 21:54:03 -0500

    change direction of nvim config to IDE.
    This commits adds treesitter for highlight, some indents identifiers
    and minor tweaks to config.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,18 +1,18 @@
 vim.cmd("let g:netrw_liststyle = 3") -- use tree style while in "file searching move" 
 
 local opt = vim.opt -- local variable
-
-
 opt.relativenumber = true -- relative number to a line
 opt.number = true -- number of current line
 
 --tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs
-opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces 
 opt.autoindent = true -- copy indent from current line when starting a new one
+opt.smarttab = true
 
 opt.wrap = false
+opt.tw=80
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
