7942c614ac92ad1d984b006fac86040310da35d8
sva-alt <175239487+sva-alt@users.noreply.github.com>
2024-11-18 01:04:48 -0500

    add current desktip config for vscode.

---
+++ vscode/extension.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+code --install-extension sainnhe.gruvbox-material
+code --install-extension equinusocio.vsc-material-theme-icons
+code --install-extension vscodevim.vim
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-dotnettools.csdevkit
+
+echo extensions installed!
+
