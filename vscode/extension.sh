14d7273c4b81feceac77fdfa6ffdcfc452c1e8fc
sva-alt <175239487+sva-alt@users.noreply.github.com>
2024-12-04 09:48:34 -0500

    add jupyter install to vscode extensions

--- vscode/extension.sh
+++ vscode/extension.sh
@@ -7,6 +7,8 @@ code --install-extension vscodevim.vim
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-toolsai.jupyter
+
 
 echo extensions installed!
 
