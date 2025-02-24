c1f0a186ed26e2958b7faaa6c900ffedbb152b4a
sva-alt <175239487+sva-alt@users.noreply.github.com>
2025-02-24 12:03:50 -0500

    change vscode to up-to-date configs

--- vscode/extension.sh
+++ vscode/extension.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 
 
-code --install-extension sainnhe.gruvbox-material
 code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension vscodevim.vim
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-dotnettools.csdevkit
 code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension github.copilot
+code --install-extension ritwickdey.liveserver
+code --install-extension samplavigne.p5-vscode
+code --install-extension daohong-emilio.yash
 
 
 echo extensions installed!
